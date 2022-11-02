dc8469c0d812672b097a5f103580dd725e80755a
DavitGe <davitge303@gmail.com>
2022-11-02 23:05:06 +0400

    Setting react-router-dom settings

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
+import Navbar from "./components/Navbar";
+
 import ScrollToTop from "./helpers/ScrollToTop";
 
 import HomePage from "./pages/HomePage";
@@ -7,12 +10,20 @@ import HomePage from "./pages/HomePage";
 function App() {
   return (
     <Router>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+      <Wrapper>
+        <ScrollToTop />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Wrapper>
     </Router>
   );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  padding: 32px 36px 52px 32px;
+  min-height: 100vh;
+`;
