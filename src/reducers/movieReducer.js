2d234f1db6bb88b67fae9f88bbccb490104bfdac
DavitGe <davitge303@gmail.com>
2022-11-02 18:48:39 +0400

    Setting defaults

---
+++ src/reducers/movieReducer.js
@@ -0,0 +1,10 @@
+import initialState from "../data.json";
+
+const movieReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default movieReducer;
